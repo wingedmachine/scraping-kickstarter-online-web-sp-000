ba100743d5c1e99a33f318f226a2dd4d8c9ebce7
Wheeler Pryor <wheeler.pryor@gmail.com>
2018-10-24 17:41:23 +0000

    automatically backed up by learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -3,5 +3,6 @@ require 'pry'
 
 def create_project_hash
   kickstarter = Nokogiri::HTML(File.read('fixtures/kickstarter.html'))
-  binding.pry
+  # projects: kickstarter.css("li.project.grid_4")
+
 end
